c0c73f95ad0ebbe2deb13e8d63afa869c3df1ad6
Lukas Stuart-Fry <lstuartfry@gmail.com>
2024-12-17 07:05:55 -0800

    removes unwanted nextjs boilerplate, adds svgr compatability

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,5 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+  content: ["./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
 } satisfies Config;
