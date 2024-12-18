d36f8697a8ca2d8eee5db3d8abc93fea106b1d98
Lukas Stuart-Fry <lstuartfry@gmail.com>
2024-12-17 19:33:07 -0800

    adds date-fns, formats album release date

--- src/components/albums/AlbumShow.tsx
+++ src/components/albums/AlbumShow.tsx
@@ -1,3 +1,4 @@
+import { format } from "date-fns";
 import { fetchAlbum } from "@/actions";
 import Image from "next/image";
 import AlbumTrack from "./AlbumTrack";
@@ -6,6 +7,12 @@ export default async function AlbumShow({ albumId }: { albumId: string }) {
   const { results } = await fetchAlbum(albumId);
   // the first result from the query will always belong to the album/collection
   const [albumMetadata, ...albumTracks] = results;
+
+  // ex: January 1st, 2025
+  const formattedReleaseDate = format(
+    new Date(albumMetadata.releaseDate),
+    "LLLL do, yyyy"
+  );
 
   return (
     <div className="pt-12 flex flex-col lg:justify-center items-center gap-4 lg:gap-12 p-4 text-white">
@@ -24,7 +31,7 @@ export default async function AlbumShow({ albumId }: { albumId: string }) {
         </span>
         <span>{albumMetadata.artistName}</span>
         <span>{albumMetadata.trackCount} track(s)</span>
-        <span>{new Date(albumMetadata.releaseDate).getFullYear()}</span>
+        <span>{formattedReleaseDate}</span>
       </div>
       <div className="bg-black/40 text-white flex flex-col space-y-6 mt-6">
         {albumTracks.map((track, index) => (
