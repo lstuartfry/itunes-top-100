055770a73dd0bed979e9666dca933a0782e3f780
Lukas Stuart-Fry <lstuartfry@gmail.com>
2024-12-19 13:45:10 -0800

    cache request for album metadata

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -6,6 +6,10 @@ export type Top100ResponseData = {
   };
 };
 
+/**
+ * Fetches top 100 albums from iTunes.
+ * NOTE - iTunes API seems to only return top 80 results.
+ */
 export const fetchTopAlbums = async (): Promise<Top100ResponseData> => {
   const response = await fetch(
     "https://itunes.apple.com/us/rss/topalbums/limit=100/json"
@@ -23,11 +27,16 @@ export type AlbumResponseData = {
   results: [AlbumCollection, ...AlbumTrack[]];
 };
 
+/**
+ * Fetches single album metadata.
+ * Revalidates at most every hour.
+ */
 export const fetchAlbum = async (
   albumId: string
 ): Promise<AlbumResponseData> => {
   const response = await fetch(
-    `https://itunes.apple.com/lookup?id=${albumId}&entity=song`
+    `https://itunes.apple.com/lookup?id=${albumId}&entity=song`,
+    { next: { revalidate: 3600 } }
   );
   const data: AlbumResponseData = await response.json();
   return data;
